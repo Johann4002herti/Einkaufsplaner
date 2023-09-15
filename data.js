4cc18e5dbadd72c4c15cb54b461a7c481182e498
Johann4002herti <145139669+Johann4002herti@users.noreply.github.com>
2023-09-15 17:00:48 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+var oberliste = [
+    /*Objekt 1*/
+        {titel: "Wocheneinkauf",
+        güter: ["Brot","Wasser","Jogurt","Cola",],
+        link:0
+        },
+        
+    ]
+export {oberliste};
+
+var rezepte = [
+    /*Objekt 1*/
+        {titel: "Mein-Fit-in-den-Tag Frühstück",
+        rezept: "Müsli in Schüssel",
+        link_nr:0
+        },
+    ]
+export {rezepte};
